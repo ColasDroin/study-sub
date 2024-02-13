f8c3a81d81472f289aadbee4591da7426af71682
Colas Droin <colasdroin@gmail.com>
2024-02-13 13:23:43 +0100

    Added configuration file and started coding class

---
+++ study_sub/study_sub.py
@@ -0,0 +1,8 @@
+from typing import Self
+
+
+class StudySub:
+    def __init__(self: Self, path_tree: str, path_config: str):
+        self.path_tree = path_tree
+        self.path_config = path_config
+
